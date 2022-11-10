6b7dcd8b4e048b16ecdae11079df045af4b1381e
oscar anillo <oscarani14@gmail.com>
2022-11-10 13:46:18 -0500

    add hero component

---
+++ src/Components/hero-component.js
@@ -0,0 +1,12 @@
+export default function HeroComponent(){
+    return (
+        <section>
+            <div className="hero-wrapper">
+                <img src="images/illustration-intro.png" alt="" />
+                <h1>All your files in one secure location, accessible anywhere.</h1>
+                <p>Fylo stores all your most important files in one secure location. Access them wherever you need, share and collabotate with friends family, and co-workers.</p>
+                <button>Get Started</button>
+            </div>
+        </section>
+    )
+}
