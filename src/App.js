b656e1f4b6a1dd6939a8d7efe58c468f98927e7c
oscar anillo <oscarani14@gmail.com>
2022-11-10 14:22:38 -0500

    Commit
    add container div
    add perks component
    add data folder with perks to map through

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import NavComponent from './Components/nav-component';
+import HeroComponent from './Components/hero-component';
+import PerksComponent from './Components/perks-component';
 
 import './App.css';
-import HeroComponent from './Components/hero-component';
 
 function App() {
   return (
     <div className="App">
       <NavComponent />
       <HeroComponent />
+      <div className="container">
+        <PerksComponent />
+
+      </div>
     </div>
   );
 }
