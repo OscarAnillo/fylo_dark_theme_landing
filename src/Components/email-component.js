8501302fc04b8be9f3e26f64aa64e3ea9207761c
oscar anillo <oscarani14@gmail.com>
2022-11-11 17:58:57 -0500

    add email verification logic

--- src/Components/email-component.js
+++ src/Components/email-component.js
@@ -1,21 +1,55 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function EmailComponent(){
-    const[emailInput, setEmailInput] = useState("");
+    const[emailInput, setEmailInput] = useState({ email: ""});
+    const[formError, setFormError] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
     const changeHandler = (e) => {
-        setEmailInput(e.target.value)
+        const {name, value} = e.target;
+        setEmailInput({...emailInput, [name]:value});
     }
+
+    const validate = (value) => {
+        const error = {};
+
+        if (!value.email){
+            error.email = "An email is required"
+        } else if (!emailRegex.test(value.email)){
+            error.email = "Please enter a valid email address"
+        }  
+        return error;
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if(emailRegex.test(emailInput.email)){
+            alert(`Signed up with email: ${emailInput.email}`);
+            setEmailInput({ email: ''})
+        }
+        setFormError(validate(emailInput))
+        setIsSubmitted(true);
+    }
+    
+    useEffect(() => {
+        if(Object.keys(emailInput).length === 0 && isSubmitted){
+            console.log("email")
+        }
+    }, [emailInput, isSubmitted])
 
     return (
         <section className="email-section">
             <div>
                 <h2>Get early access today</h2>
                 <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you.</p>
-                <form>
-                    <input type="text" placeholder="email@example.com" value={emailInput} onChange={changeHandler} />
+                <form onSubmit={submitHandler}>
+                    <input type="text" name="email" placeholder="email@example.com" value={emailInput.email} onChange={changeHandler} />
                     <button>Get Started For Free</button>
                 </form>
+                {
+                    formError.email && <p className='error'>{formError.email}</p>
+                }
             </div>
         </section>
     )
